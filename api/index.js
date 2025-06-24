3b816df984e517bf0a0f1d90d5ef6234b252772e
ghanekarpayal11213 <126890237+ghanekarpayal11213@users.noreply.github.com>
2025-06-24 11:59:06 +0530

    Updated port no

--- api/index.js
+++ api/index.js
@@ -7,3 +7,7 @@ app.use(cors({
   origin: "https://holkarians-ave-college-space.vercel.app/",  
   credentials: true
 }));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
