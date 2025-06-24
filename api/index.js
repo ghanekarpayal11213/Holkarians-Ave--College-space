e3ce7e4e8302d7875cccde564734c3ddfee3c3fe
ghanekarpayal11213 <126890237+ghanekarpayal11213@users.noreply.github.com>
2025-06-24 11:17:23 +0530

    Updated frontend vercel connection

--- api/index.js
+++ api/index.js
@@ -1,2 +1,9 @@
 const app  = require('../index')
 module.exports = app
+
+import cors from 'cors';
+
+app.use(cors({
+  origin: "https://holkarians-ave-college-space.vercel.app/",  
+  credentials: true
+}));
